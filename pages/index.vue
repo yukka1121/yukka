0c64771cd1cdbf2c399fd4db78caa3456b4a15af
yukari <yu.kiku6986@gmail.com>
2020-04-18 11:01:57 +0900

    アプリ作成

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+　 <v-form>
+    <v-container>
+      <v-row>  
+        <v-col cols="12" sm="6" md="3">
+          <v-text-field
+            label="Regular"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
