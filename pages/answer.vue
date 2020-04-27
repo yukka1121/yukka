d1ce96071bc9138a5a6c8a469f360f5a4cf66356
yukari <yu.kiku6986@gmail.com>
2020-04-28 00:31:10 +0900

    画面遷移

---
+++ pages/answer.vue
@@ -0,0 +1,14 @@
+<template>
+   <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            label="ぬれ手で（に）粟"
+            single-line
+          ></v-text-field>
+        </v-col>
+      </v-row>     
+    </v-container> 
+   </v-form>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,34 @@
 <template>
-  <v-app>
-　 <v-container >
-    <v-card
-     class="mx-auto"
-     max-width="344"
-  >
-    　<v-card-text>
-      <div>問題</div>
-      <p class="display-1 text--primary">
-        be•nev•o•lent
-      </p>
-      <p>adjective</p>
-      <div class="text--primary">
-        well meaning and kindly.<br>
-        "a benevolent smile"
-      </div>
-    　</v-card-text>
-    　<v-card-actions>  
-        <v-row align="center">
-         <v-col class="text-center" cols="12" v-for="n in 4" :key="n">
+   <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            label="苦労せず大きな利益を得る事"
+            single-line
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            label=""
+            single-line
+          ></v-text-field>
+        </v-col>        
+      </v-row>  
+      <v-row align="center">
+        <v-col class="text-center" cols="12" sm="4">
           <div class="my-2">
-            <v-btn depressed large>Normal</v-btn>
+            <v-btn depressed large @click="$router.push('answer')">answerへ</v-btn>
           </div>
-         </v-col>
-        </v-row>  
-    　</v-card-actions>
-  　</v-card>
-  </v-container> 
-  </v-app>
+        </v-col>
+      </v-row>  
+    </v-container> 
+   </v-form>
+
 </template>
 
 <script>
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  width: 100px;
-  min-height: 150vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
