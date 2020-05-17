230ec3788ab5473af9370aff4cbeea8829d1bf5a
yukari <yu.kiku6986@gmail.com>
2020-05-17 20:33:52 +0900

    add

--- pages/answer.vue
+++ pages/answer.vue
@@ -1,22 +1,14 @@
 <template>
-   <v-form>
+  <v-form>
     <v-container>
       <v-row>
-        <div class="my-2">
-          <nuxt-link to="/answer">答え</nuxt-link>
-        </div> 
         <v-col cols="12" sm="6">
-          <v-text-field
-            label="ぬれ手で（に）粟"
-            single-line
-          ></v-text-field>
-
+          <v-text-field v-model="answerTxt" label="回答" :change="answerChanged()" required clearable></v-text-field>
         </v-col>
-      </v-row>     
-    </v-container> 
-   </v-form>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
-
 </script>
